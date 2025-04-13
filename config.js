5596be50b50531a5114caa077e1df3308a203be6
luvingg <luvriott@gmail.com>
2025-04-13 21:33:29 +0700

    fist

---
+++ config.js
@@ -0,0 +1,16 @@
+const { ActivityType } = require('discord.js');
+
+module.exports = {
+  ownerId: '580771765190656000',
+  status: {
+    rotateDefault: [
+      { name: 'ngủ để quên đi nỗi buồn', type: ActivityType.Watching },
+      { name: 'mong rằng hoa sẽ mọc ở nơi buồn bã nhất trong em', type: ActivityType.Playing },
+      { name: 'để anh dạy em cách yêu', type: ActivityType.Streaming, url: 'https://discord.gg/SM2uTDjxw3' },
+      { name: 'meomeomeo meomeo', type: ActivityType.Listening },
+    ],
+    songStatus: true
+  },
+  spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
+}
